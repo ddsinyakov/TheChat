ae2c48ab8232349e15bd2e82ef2b6424d70f1105
Дима Синяков <dimasinyakovwork@gmail.com>
2022-10-14 17:55:54 +0300

    feat: Add CORS policy

--- TheChat/Program.cs
+++ TheChat/Program.cs
@@ -4,25 +4,35 @@ using Microsoft.AspNetCore.Builder;
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager config = builder.Configuration; // access to configuration
 
+// -------------------------------------------------------------------------------------------
 // Add services to the container.
+// -------------------------------------------------------------------------------------------
 
 builder.Services.AddControllers();          // asp .net controllers
 builder.Services.AddEndpointsApiExplorer(); // search for endpoints to show in swagger 
 builder.Services.AddSwaggerGen();           // swagger interface 
 
+// cross domen requests
+// TODO: change accessibility onlu for frontend pages
+builder.Services.AddCors();                 
+
 var app = builder.Build();
- 
+
+
+
+
+// -------------------------------------------------------------------------------------------
 // Configure the HTTP request pipeline.
+//-------------------------------------------------------------------------------------------
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseCors(builder => builder.AllowAnyOrigin());
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
