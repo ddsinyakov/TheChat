cbc844bcf0276eaa8c863c4f5ea1119af2b46d15
Дима Синяков <dimasinyakovwork@gmail.com>
2022-10-19 14:07:50 +0300

    feat: Add AuthenticationController with Register method to add user to Database. Add UserDTO to store data about user before being added to Database

---
+++ TheChat/Controllers/AuthenticationController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Linq.Expressions;
+using TheChat.Models.DTO;
+using TheChat.Models.Entities;
+using TheChat.Services.DataBase.UserDAO;
+using TheChat.Services.Hash;
+
+namespace TheChat.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : Controller
+    {
+        private IUserDao _userDao { get; init; }
+        private IHashService _hashService { get; init; }
+
+        public AuthenticationController(IUserDao userDao, IHashService hashService)
+        {
+            _userDao = userDao;
+            _hashService = hashService;
+        }
+
+        #region Registration
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(
+                [FromBody] UserRegisterDTO toRegister,
+                [FromServices] IOptions<ApiBehaviorOptions> apiBehaviorOptions)
+        {
+            // Check the validation of request parameters
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            Boolean exist;
+
+            // Used insted of BadRequest() to properly generate the responce headers when Error occured 
+            var factory = apiBehaviorOptions.Value.InvalidModelStateResponseFactory;
+
+            // Check if there is no users in Database with the same login
+            exist = await CheckIfExist(
+                predicate: user => user.Login == toRegister.Login,
+                key: nameof(toRegister.Login),
+                errorMessage: "Login is used"
+            );
+
+            if (exist)
+                return factory(ControllerContext);
+
+            // Check if there is no users in Database with the same email
+            exist = await CheckIfExist(
+                predicate: user => user.Email == toRegister.Email,
+                key: nameof(toRegister.Email),
+                errorMessage: "Email is used"
+            );
+
+            if (exist)
+                return factory(ControllerContext);
+
+            // Generate new user and add it to database
+            User newUser = await AddNewUser(toRegister);
+
+            return Ok();
+
+        }
+
+        [NonAction] // Used inside class to figure out if there is user that correspondes given predicate already in database
+        private async Task<Boolean> CheckIfExist(Expression<Func<User, Boolean>> predicate, String key, String errorMessage)
+        {
+            User? checkIfExist = await _userDao.GetUserAsync(predicate);
+
+            if (checkIfExist is not null) return true;
+
+            ModelState.AddModelError(key, errorMessage); // Adds model error to turn back in responce
+            return false;
+        }
+
+        [NonAction] // Used inside class to create user entity and add it database
+        private async Task<User> AddNewUser(UserRegisterDTO toAdd)
+        {
+            String salt = _hashService.Hash(_hashService.GenerateSalt());
+            String hashedPassword = _hashService.Hash(toAdd.Password + salt);
+
+            User newUser = new User()
+            {
+                Login = toAdd.Login,
+                FirstName = toAdd.FirstName,
+                SecondName = toAdd.SecondName,
+                Email = toAdd.Email,
+                Password = hashedPassword,
+                Salt = salt,
+                CreatedDate = DateTime.Now,
+                LastLoginDate = DateTime.Now
+            };
+
+            await _userDao.AddUserAsync(newUser);
+
+            return newUser;
+        }
+
+        #endregion
+    }
+}
---
+++ TheChat/Models/DTO/UserRegisterDTO.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheChat.Models.DTO
+{
+    public class UserRegisterDTO
+    {
+        [MaxLength(30, ErrorMessage = "First name is too long")]
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; } = String.Empty;
+
+
+        [MaxLength(30, ErrorMessage = "Second name is too long")]
+        [Required(ErrorMessage = "Second Name is required")]
+        public string SecondName { get; set; } = String.Empty;
+
+
+        [MaxLength(30, ErrorMessage = "Login is too long")]
+        [Required(ErrorMessage = "Login is required")]
+        public string Login { get; set; } = String.Empty;
+
+
+        [EmailAddress(ErrorMessage = "Invalid address")]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; } = String.Empty;
+
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; } = String.Empty;
+
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords are different")]
+        [Required(ErrorMessage = "Password repeat is required")]
+        public string ConfirmPassword { get; set; } = String.Empty;
+    }
+}
